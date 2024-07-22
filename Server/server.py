510f19b8ff4a6d2d16486f943767a6ccddaf27e4
Tomoaki Iwaya <iwayatomoaki@gmail.com>
2024-07-21 21:48:39 -0400

    milvus-lite is not supported nativaly on windows

---
+++ Server/server.py
@@ -0,0 +1,10 @@
+from pymilvus import MilvusClient
+
+client = MilvusClient("milvus_demo.db")
+
+if client.has_collection(collection_name="demo_collection"):
+    client.drop_collection(collection_name="demo_collection")
+client.create_collection(
+    collection_name="demo_collection",
+    dimension=768,  # The vectors we will use in this demo has 768 dimensions
+)
