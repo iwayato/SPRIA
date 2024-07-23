c48c05ec97cd9e56413e800e6d5dff4a23fcbf24
Tomoaki Iwaya <iwayatomoaki@gmail.com>
2024-07-22 23:51:39 -0400

    feat: added Milvus simple example to understand core concepts

--- Server/server.py
+++ Server/server.py
@@ -1,10 +1,54 @@
 from pymilvus import MilvusClient
+from pymilvus import model
 
 client = MilvusClient("milvus_demo.db")
 
 if client.has_collection(collection_name="demo_collection"):
     client.drop_collection(collection_name="demo_collection")
-client.create_collection(
-    collection_name="demo_collection",
-    dimension=768,  # The vectors we will use in this demo has 768 dimensions
+else:
+    client.create_collection(
+        collection_name="demo_collection",
+        dimension=768,  # The vectors we will use in this demo has 768 dimensions
+    )
+    
+# If connection to https://huggingface.co/ failed, uncomment the following path
+# import os
+# os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+
+# This will download a small embedding model "paraphrase-albert-small-v2" (~50MB).
+embedding_fn = model.DefaultEmbeddingFunction()
+
+# Text strings to search from.
+docs = [
+    "Artificial intelligence was founded as an academic discipline in 1956.",
+    "Alan Turing was the first person to conduct substantial research in AI.",
+    "Born in Maida Vale, London, Turing was raised in southern England.",
+]
+
+vectors = embedding_fn.encode_documents(docs)
+# The output vector has 768 dimensions, matching the collection that we just created.
+print("Dimensions:", embedding_fn.dim, vectors[0].shape)  # Dim: 768 (768,)
+
+# Each entity has id, vector representation, raw text, and a subject label that we use
+# to demo metadata filtering later.
+data = [{"id": i, "vector": vectors[i], "text": docs[i], "subject": "history"} for i in range(len(vectors))]
+
+print("Data has", len(data), "entities, each with fields: ", data[0].keys())
+print("Vector dim:", len(data[0]["vector"]))
+
+res = client.insert(collection_name="demo_collection", data=data)
+
+print(res)
+
+query_vectors = embedding_fn.encode_queries(["Who is Alan Turing?"])
+# If you don't have the embedding function you can use a fake vector to finish the demo:
+# query_vectors = [ [ random.uniform(-1, 1) for _ in range(768) ] ]
+
+res = client.search(
+    collection_name="demo_collection",  # target collection
+    data=query_vectors,  # query vectors
+    limit=2,  # number of returned entities
+    output_fields=["text", "subject"],  # specifies fields to be returned
 )
+
+print(res)
