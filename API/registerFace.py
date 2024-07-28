5aced427babcd71cf91bc2f97a239460342deb4b
Tomoaki Iwaya <iwayatomoaki@gmail.com>
2024-07-28 13:57:15 -0400

    feat: added basic registerFace code example

--- API/registerFace.py
+++ API/registerFace.py
@@ -1,6 +1,12 @@
 import cv2 as cv
 import dlib
 import helpers
+import requests
+import json
+from datetime import datetime, date
+
+api_url = "http://127.0.0.1:5000/insert_vector"
+headers =  {"Content-Type":"application/json"}
 
 video = cv.VideoCapture(0)
 faceDetector = dlib.get_frontal_face_detector()
@@ -23,6 +29,14 @@ while(True):
         landmarks = shapePredictor(imgCopy, faceDetected)
         faceChip = dlib.get_face_chip(imgCopy, landmarks)
         faceEncoding = helpers.processFaceChip(faceChip)
+        data = {
+            'faces': faceEncoding,
+            'counter': 0,
+            'last_seen': datetime.now(),
+            'creation_date': date.today()
+        }
+        response = requests.post(api_url, data=json.dumps(data, default=str), headers=headers)
+        print(response.json())
         cv.imshow('Face chip', faceChip)
         
     if cv.waitKey(1) & 0xFF == ord('q'): 
